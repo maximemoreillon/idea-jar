1257328fae6f5db8756612a27035ce972ef97fa7
Maxime Moreillon <m.moreillon@gmail.com>
2026-03-22 09:55:53 +0900

    db test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
+<script lang="ts">
+  import { db } from "$lib/firebase";
+
+  import { collection, getDocs } from "firebase/firestore";
+
+  async function test() {
+    const querySnapshot = await getDocs(collection(db, "test"));
+
+    console.log(querySnapshot.docs[1].data());
+
+    // querySnapshot.forEach((doc) => {
+    //   console.log(doc.data());
+    // });
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<button onclick={test}>Test</button>
