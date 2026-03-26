e28f7d7ef570ca0f74a2f4308deea5efcd3ae402
Maxime Moreillon <m.moreillon@gmail.com>
2026-03-26 17:08:26 +0900

    simplified a bit

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,26 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import type { QueryDocumentSnapshot } from "firebase/firestore";
+
+export function random_number_between_0_and(x: number) {
+  return Math.floor(Math.random() * x);
+}
+
+export function pick_random_author(database_records: QueryDocumentSnapshot[]) {
+  const distinct_authors: string[] = [
+    ...new Set(database_records.map((r) => r.data().author || "anonymous")),
+  ];
+
+  const number_of_authors = distinct_authors.length;
+  const random_author_index = random_number_between_0_and(number_of_authors);
+  const picked_author = distinct_authors[random_author_index];
+
+  return picked_author;
+}
+
+export function get_ideas_of_author(
+  records: QueryDocumentSnapshot[],
+  auhtor: string,
+) {
+  return records.filter((d) => d.data().author === auhtor);
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,47 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
+  import {
+    get_ideas_of_author,
+    pick_random_author,
+    random_number_between_0_and,
+  } from "$lib";
   import IdeasCount from "$lib/components/ideasCount.svelte";
-  import { db, ideas_collection } from "$lib/firebase";
+  import { ideas_collection } from "$lib/firebase";
 
-  import { collection, getDocs } from "firebase/firestore";
+  import { getDocs } from "firebase/firestore";
 
-  let picking = $state(false);
-
-  function random_number_between_0_and(x: number) {
-    return Math.floor(Math.random() * x);
-  }
+  let fetching = $state(false);
 
   async function pick_idea() {
-    picking = true;
-
+    fetching = true;
     const database_fetch_result = await getDocs(ideas_collection); // Query the database
+    fetching = false;
+
     const database_records = database_fetch_result.docs; // Get the records (items) from the database query result
 
     // STEP 1: pick one author
+    const picked_author = pick_random_author(database_records);
 
-    // This bit looks complicated but it just make a list of distinct idea authors
-    const distinct_authors = [
-      ...new Set(database_records.map((r) => r.data().author)),
-    ];
-
-    const number_of_authors = distinct_authors.length;
-    const random_author_index = random_number_between_0_and(number_of_authors);
-    const picked_author = distinct_authors[random_author_index];
-
-    // STEP 2: pick one random idea of that author
-
-    // Filter out ideas that are not from the picked author and that are not picked yet
-    const items_of_picked_author = database_records.filter(
-      (d) => d.data().author === picked_author,
+    // STEP 2: get ideas of that author
+    const items_of_picked_author = get_ideas_of_author(
+      database_records,
+      picked_author,
     );
 
     const number_of_ideas = items_of_picked_author.length;
 
-    if (number_of_ideas > 0) {
-      const picked_idea_index = random_number_between_0_and(number_of_ideas);
-      const picked_idea = database_records[picked_idea_index];
-
-      goto(`/ideas/${picked_idea.id}`);
-    } else {
+    // Deal with cases where there are no ideas
+    if (number_of_ideas < 1) {
       alert("Jar is empty");
+      return; // Stop execution
     }
 
-    picking = false;
+    // STEP 3: Pick a random idea from that of the picked author
+    const picked_idea_index = random_number_between_0_and(number_of_ideas);
+    const picked_idea = database_records[picked_idea_index];
+
+    // Finally, move to the page of the idea
+    goto(`/ideas/${picked_idea.id}`);
   }
 </script>
 
@@ -54,6 +49,6 @@
 
 <IdeasCount />
 
-<button onclick={pick_idea} disabled={picking}>
+<button onclick={pick_idea} disabled={fetching}>
   Pick an idea from the jar
 </button>
